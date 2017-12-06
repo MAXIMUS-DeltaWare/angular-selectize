9fa5ff63f272d3a9dafe7472f3c3c53978d9d743
Kashyap Patel <kpatel@DeltaWare.com>
2017-12-06 13:25:41 -0400

    isEmpty function modified to check numeric values properly

--- dist/angular-selectize.js
+++ dist/angular-selectize.js
@@ -17,7 +17,11 @@ angular.module('selectize', []).value('selectizeConfig', {}).directive("selectiz
       scope.config = scope.config || {};
 
       var isEmpty = function(val) {
-        return val === undefined || val === null || !val.length; //support checking empty arrays
+        if(Array.isArray(val)) {
+          return (val.length > 0) ? false : true;
+        }
+        //call to real Angular function
+        return modelCtrl.$isEmpty(val); 
       };
 
       var toggle = function(disabled) {
